85b26a5987462129d1914cc06047e39d43d6f87f
PixelPioneer2003 <codinglit2908@gmail.com>
2025-06-30 00:06:50 +0530

    adding the link of backend after deployment

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,4 +1,5 @@
 // frontend/src/socket.js
 import { io } from "socket.io-client";
-const socket = io("http://localhost:5000"); // Replace with your backend URL
+const socket = io("https://cp-battle-backend.onrender.com");
+// const socket = io("http://localhost:5000"); // Replace with your backend URL
 export default socket;
